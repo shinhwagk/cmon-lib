36cf7428544e40afa7186758d8d9cd6b4013a22c
shinhwagk <shinhwagk@outlook.com>
2018-03-09 00:21:27 +0800

    test

--- src/Steps/FilterStep.ts
+++
@@ -1,8 +0,0 @@
-import { IEmitDownStream, IStep } from "../step";
-
-function FilterStep<P, M>(name: string, filterFunc: (elem: M) => boolean): IStep {
-    name = "Filter-" + name;
-    const stepProcess = (emit: IEmitDownStream) =>
-        (point: P, elem: M) => { if (filterFunc(elem)) { emit(point, elem); } };
-    return { name, stepProcess };
-}
--- src/Steps/PrintStep.ts
+++
@@ -1,7 +0,0 @@
-import { IStep } from "../step";
-
-function PrintStep<P, M>(name: string, PrintFunc: (point: P, elemt: M) => void): IStep {
-    name = "Print-" + name;
-    const stepProcess = (emit: any) => (point: P, elem: M) => PrintFunc(point, elem);
-    return { name, stepProcess };
-}
